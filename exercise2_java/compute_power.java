97bc207bc5eb72b8dc8d9e09db6cec83b4f4f292
PulsarBane <100311500+PulsarBane@users.noreply.github.com>
2022-08-02 16:58:34 -0400

    Updating for better indentation and format

--- exercise2_java/compute_power.java
+++ exercise2_java/compute_power.java
@@ -4,27 +4,28 @@ import java.util.Scanner;
 
 
 public class compute_power {
-		int compute(int num, int exp) {
-		int res = (int) Math.pow(num, exp);
-		return res;
+	
+	int compute(int num, int exp) {
+	int res = (int) Math.pow(num, exp);
+	return res;
 	}
 		
-		public static void main(String[] args){
+public static void main(String[] args){
 			
-			compute_power pow = new compute_power();
+	compute_power pow = new compute_power();
 			
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter number: ");
-			int numA = sc.nextInt();
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter number: ");
+	int numA = sc.nextInt();
 			
-			Scanner sc1 = new Scanner(System.in);
-			System.out.println("Enter exponent: ");
-			int numB = sc.nextInt();
+	Scanner sc1 = new Scanner(System.in);
+	System.out.println("Enter exponent: ");
+	int numB = sc.nextInt();
 			
 			
-			int result = pow.compute(numA, numB);
-			System.out.print("Result is: " + result);
+	int result = pow.compute(numA, numB);
+	System.out.print("Result is: " + result);
 			
-		}
+	}
 
 }
