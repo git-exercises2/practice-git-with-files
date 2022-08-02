0fd048a3b05017a91a55b7da178051737b500b57
PulsarBane <100311500+PulsarBane@users.noreply.github.com>
2022-08-02 16:52:14 -0400

    Adding user input method

--- exercise2_java/compute_power.java
+++ exercise2_java/compute_power.java
@@ -1,16 +1,30 @@
 package java_basics;
 import java.lang.Math;
+import java.util.Scanner;
 
 
 public class compute_power {
-	public static void main(String[] args) {
-
-	}
-
 		int compute(int num, int exp) {
-		    double res = Math.pow(num, exp);
-
-		return (int) (res);
+		int res = (int) Math.pow(num, exp);
+		return res;
 	}
-}
 		
+		public static void main(String[] args){
+			
+			compute_power pow = new compute_power();
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter number: ");
+			int numA = sc.nextInt();
+			
+			Scanner sc1 = new Scanner(System.in);
+			System.out.println("Enter exponent: ");
+			int numB = sc.nextInt();
+			
+			
+			int result = pow.compute(numA, numB);
+			System.out.print("Result is: " + result);
+			
+		}
+
+}
